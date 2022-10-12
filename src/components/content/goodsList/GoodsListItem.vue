fdbd5085c19598372b71a80e45ad734cad39774b
xiaoli112358 <1246468641@qq.com>
2022-10-12 22:32:11 +0800

    first commit

---
+++ src/components/content/goodsList/GoodsListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="goods-list-item">
+    <img :src="goodsItem.show.img" alt="">
+    <div class="goods-info">
+      <p>{{goodsItem.title}}</p>
+      <span class="price">{{goodsItem.price}}</span>
+      <span class="cfav">{{goodsItem.cfav}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GoodsListItem",
+    props: {
+      goodsItem: {
+        type: Object,
+        default() {
+          return {}
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
