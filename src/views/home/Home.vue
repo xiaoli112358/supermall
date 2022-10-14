3bbef9d2f5fa2d45e6ceb9fce90566a625adf7b2
xiaoli112358 <1246468641@qq.com>
2022-10-14 17:08:39 +0800

    首页给商品添加scroll滚动

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,117 +1,117 @@
 <template>
   <div id="home">
-    <navBar class="home-nav">
-      <div slot="center">购物街</div>
-    </navBar>
-    <HomeSwiper :banners="banners"></HomeSwiper>
-    <Recommend :recommends="recommends"></Recommend>
-    <feature-view></feature-view>
-    <tab-control :titles="['流行','新款','精选']"
-                 class="tab-control"
-                  @tabClick="tabClick">
-    </tab-control>
-    <goods-list :goods="showGoods"></goods-list>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <Scroll class="content">
+      <home-swiper :banners="banners"></home-swiper>
+      <Recommend :recommends="recommends"></Recommend>
+      <feature-view></feature-view>
+      <tab-control :titles="['流行','新款','精选']"
+                   class="tab-control"
+                   @tabClick="tabClick">
+      </tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+    </Scroll>
     <!--<ul>-->
-      <!--<li>列表1</li>-->
-      <!--<li>列表2</li>-->
-      <!--<li>列表3</li>-->
-      <!--<li>列表4</li>-->
-      <!--<li>列表5</li>-->
-      <!--<li>列表6</li>-->
-      <!--<li>列表7</li>-->
-      <!--<li>列表8</li>-->
-      <!--<li>列表9</li>-->
-      <!--<li>列表10</li>-->
-      <!--<li>列表11</li>-->
-      <!--<li>列表12</li>-->
-      <!--<li>列表13</li>-->
-      <!--<li>列表14</li>-->
-      <!--<li>列表15</li>-->
-      <!--<li>列表16</li>-->
-      <!--<li>列表17</li>-->
-      <!--<li>列表18</li>-->
-      <!--<li>列表19</li>-->
-      <!--<li>列表20</li>-->
-      <!--<li>列表21</li>-->
-      <!--<li>列表22</li>-->
-      <!--<li>列表23</li>-->
-      <!--<li>列表24</li>-->
-      <!--<li>列表25</li>-->
-      <!--<li>列表26</li>-->
-      <!--<li>列表27</li>-->
-      <!--<li>列表28</li>-->
-      <!--<li>列表29</li>-->
-      <!--<li>列表30</li>-->
-      <!--<li>列表31</li>-->
-      <!--<li>列表32</li>-->
-      <!--<li>列表33</li>-->
-      <!--<li>列表34</li>-->
-      <!--<li>列表35</li>-->
-      <!--<li>列表36</li>-->
-      <!--<li>列表37</li>-->
-      <!--<li>列表38</li>-->
-      <!--<li>列表39</li>-->
-      <!--<li>列表40</li>-->
-      <!--<li>列表41</li>-->
-      <!--<li>列表42</li>-->
-      <!--<li>列表43</li>-->
-      <!--<li>列表44</li>-->
-      <!--<li>列表45</li>-->
-      <!--<li>列表46</li>-->
-      <!--<li>列表47</li>-->
-      <!--<li>列表48</li>-->
-      <!--<li>列表49</li>-->
-      <!--<li>列表50</li>-->
-      <!--<li>列表51</li>-->
-      <!--<li>列表52</li>-->
-      <!--<li>列表53</li>-->
-      <!--<li>列表54</li>-->
-      <!--<li>列表55</li>-->
-      <!--<li>列表56</li>-->
-      <!--<li>列表57</li>-->
-      <!--<li>列表58</li>-->
-      <!--<li>列表59</li>-->
-      <!--<li>列表60</li>-->
-      <!--<li>列表61</li>-->
-      <!--<li>列表62</li>-->
-      <!--<li>列表63</li>-->
-      <!--<li>列表64</li>-->
-      <!--<li>列表65</li>-->
-      <!--<li>列表66</li>-->
-      <!--<li>列表67</li>-->
-      <!--<li>列表68</li>-->
-      <!--<li>列表69</li>-->
-      <!--<li>列表70</li>-->
-      <!--<li>列表71</li>-->
-      <!--<li>列表72</li>-->
-      <!--<li>列表73</li>-->
-      <!--<li>列表74</li>-->
-      <!--<li>列表75</li>-->
-      <!--<li>列表76</li>-->
-      <!--<li>列表77</li>-->
-      <!--<li>列表78</li>-->
-      <!--<li>列表79</li>-->
-      <!--<li>列表80</li>-->
-      <!--<li>列表81</li>-->
-      <!--<li>列表82</li>-->
-      <!--<li>列表83</li>-->
-      <!--<li>列表84</li>-->
-      <!--<li>列表85</li>-->
-      <!--<li>列表86</li>-->
-      <!--<li>列表87</li>-->
-      <!--<li>列表88</li>-->
-      <!--<li>列表89</li>-->
-      <!--<li>列表90</li>-->
-      <!--<li>列表91</li>-->
-      <!--<li>列表92</li>-->
-      <!--<li>列表93</li>-->
-      <!--<li>列表94</li>-->
-      <!--<li>列表95</li>-->
-      <!--<li>列表96</li>-->
-      <!--<li>列表97</li>-->
-      <!--<li>列表98</li>-->
-      <!--<li>列表99</li>-->
-      <!--<li>列表100</li>-->
+      <!--<li>分类列表1</li>-->
+      <!--<li>分类列表2</li>-->
+      <!--<li>分类列表3</li>-->
+      <!--<li>分类列表4</li>-->
+      <!--<li>分类列表5</li>-->
+      <!--<li>分类列表6</li>-->
+      <!--<li>分类列表7</li>-->
+      <!--<li>分类列表8</li>-->
+      <!--<li>分类列表9</li>-->
+      <!--<li>分类列表10</li>-->
+      <!--<li>分类列表11</li>-->
+      <!--<li>分类列表12</li>-->
+      <!--<li>分类列表13</li>-->
+      <!--<li>分类列表14</li>-->
+      <!--<li>分类列表15</li>-->
+      <!--<li>分类列表16</li>-->
+      <!--<li>分类列表17</li>-->
+      <!--<li>分类列表18</li>-->
+      <!--<li>分类列表19</li>-->
+      <!--<li>分类列表20</li>-->
+      <!--<li>分类列表21</li>-->
+      <!--<li>分类列表22</li>-->
+      <!--<li>分类列表23</li>-->
+      <!--<li>分类列表24</li>-->
+      <!--<li>分类列表25</li>-->
+      <!--<li>分类列表26</li>-->
+      <!--<li>分类列表27</li>-->
+      <!--<li>分类列表28</li>-->
+      <!--<li>分类列表29</li>-->
+      <!--<li>分类列表30</li>-->
+      <!--<li>分类列表31</li>-->
+      <!--<li>分类列表32</li>-->
+      <!--<li>分类列表33</li>-->
+      <!--<li>分类列表34</li>-->
+      <!--<li>分类列表35</li>-->
+      <!--<li>分类列表36</li>-->
+      <!--<li>分类列表37</li>-->
+      <!--<li>分类列表38</li>-->
+      <!--<li>分类列表39</li>-->
+      <!--<li>分类列表40</li>-->
+      <!--<li>分类列表41</li>-->
+      <!--<li>分类列表42</li>-->
+      <!--<li>分类列表43</li>-->
+      <!--<li>分类列表44</li>-->
+      <!--<li>分类列表45</li>-->
+      <!--<li>分类列表46</li>-->
+      <!--<li>分类列表47</li>-->
+      <!--<li>分类列表48</li>-->
+      <!--<li>分类列表49</li>-->
+      <!--<li>分类列表50</li>-->
+      <!--<li>分类列表51</li>-->
+      <!--<li>分类列表52</li>-->
+      <!--<li>分类列表53</li>-->
+      <!--<li>分类列表54</li>-->
+      <!--<li>分类列表55</li>-->
+      <!--<li>分类列表56</li>-->
+      <!--<li>分类列表57</li>-->
+      <!--<li>分类列表58</li>-->
+      <!--<li>分类列表59</li>-->
+      <!--<li>分类列表60</li>-->
+      <!--<li>分类列表61</li>-->
+      <!--<li>分类列表62</li>-->
+      <!--<li>分类列表63</li>-->
+      <!--<li>分类列表64</li>-->
+      <!--<li>分类列表65</li>-->
+      <!--<li>分类列表66</li>-->
+      <!--<li>分类列表67</li>-->
+      <!--<li>分类列表68</li>-->
+      <!--<li>分类列表69</li>-->
+      <!--<li>分类列表70</li>-->
+      <!--<li>分类列表71</li>-->
+      <!--<li>分类列表72</li>-->
+      <!--<li>分类列表73</li>-->
+      <!--<li>分类列表74</li>-->
+      <!--<li>分类列表75</li>-->
+      <!--<li>分类列表76</li>-->
+      <!--<li>分类列表77</li>-->
+      <!--<li>分类列表78</li>-->
+      <!--<li>分类列表79</li>-->
+      <!--<li>分类列表80</li>-->
+      <!--<li>分类列表81</li>-->
+      <!--<li>分类列表82</li>-->
+      <!--<li>分类列表83</li>-->
+      <!--<li>分类列表84</li>-->
+      <!--<li>分类列表85</li>-->
+      <!--<li>分类列表86</li>-->
+      <!--<li>分类列表87</li>-->
+      <!--<li>分类列表88</li>-->
+      <!--<li>分类列表89</li>-->
+      <!--<li>分类列表90</li>-->
+      <!--<li>分类列表91</li>-->
+      <!--<li>分类列表92</li>-->
+      <!--<li>分类列表93</li>-->
+      <!--<li>分类列表94</li>-->
+      <!--<li>分类列表95</li>-->
+      <!--<li>分类列表96</li>-->
+      <!--<li>分类列表97</li>-->
+      <!--<li>分类列表98</li>-->
+      <!--<li>分类列表99</li>-->
+      <!--<li>分类列表100</li>-->
     <!--</ul>-->
   </div>
 </template>
@@ -124,6 +124,7 @@
   import navBar from 'components/common/navBar/NavBar'
   import TabControl from 'components/content/tabcontrol/TabControl'
   import GoodsList from 'components/content/goodsList/GoodsList'
+  import Scroll from 'components/common/scroll/Scroll'
 
   import {getHomeMultiData, getHomeGoods} from "network/home";
 
@@ -135,7 +136,8 @@
       FeatureView,
       navBar,
       TabControl,
-      GoodsList
+      GoodsList,
+      Scroll
     },
     data() {
       return {
@@ -181,7 +183,6 @@
             break
         }
       },
-
       /**
        * 网络请求相关的方法
        */
@@ -207,9 +208,9 @@
 
 <style scoped>
   #home {
-    padding-top: 44px;
-    /*height: 100vh;*/
-    /*position: relative;*/
+    /*padding-top: 44px;*/
+    height: 100vh;
+    position: relative;
   }
 
   .home-nav {
@@ -229,5 +230,11 @@
     position: sticky;
     top: 44px;
   }
-
+  .content{
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
 </style>
