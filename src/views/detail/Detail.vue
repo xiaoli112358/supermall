c21ac565203afed65f9c51b934b0a89689b022f7
xiaoli112358 <1246468641@qq.com>
2022-10-16 18:08:19 +0800

    店铺信息开发

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -3,6 +3,7 @@
     <detail-nav-bar/>
     <detail-swiper :topImages="topImages"></detail-swiper>
     <detail-base-info :goods="goods"/>
+    <detail-shop-info :shop="shop"></detail-shop-info>
   </div>
 </template>
 
@@ -10,35 +11,40 @@
   import DetailNavBar from 'views/detail/childComps/DetailNavBar'
   import DetailSwiper from 'views/detail/childComps/DetailSwiper'
   import DetailBaseInfo from 'views/detail/childComps/DetailBaseInfo'
+  import DetailShopInfo from 'views/detail/childComps/DetailShopInfo'
 
-  import {getDetail,Goods} from "network/detail"
+  import {getDetail, Goods,Shop} from "network/detail"
 
   export default {
     name: "detail",
     components: {
       DetailNavBar,
       DetailSwiper,
-      DetailBaseInfo
+      DetailBaseInfo,
+      DetailShopInfo
     },
     data() {
       return {
         iid: null,
         topImages: [],
-        goods:{}
+        goods: {},
+        shop: {},
       }
     },
     created() {
       // 1.保存传入的iid
       this.iid = this.$route.params.iid
-      // 2.根据iid请求详情数据
+      // 1.根据iid请求详情数据
       getDetail(this.iid).then(res => {
         // console.log(res);
         const data = res.result;
         console.log(data.itemInfo.topImages)
         this.topImages = data.itemInfo.topImages
 
-        // 3.获取商品信息
+        // 2.获取商品信息
         this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+        // 3.创建店铺信息的对象
+        this.shop = new Shop(data.shopInfo)
       })
 
     }
