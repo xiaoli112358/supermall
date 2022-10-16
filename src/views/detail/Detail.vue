cd8b66f960206fef4be62bd00d0fa1a1ffd42e3c
xiaoli112358 <1246468641@qq.com>
2022-10-16 17:50:08 +0800

    详细页轮播图下面内容的开发

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,36 +2,45 @@
   <div class="detail">
     <detail-nav-bar/>
     <detail-swiper :topImages="topImages"></detail-swiper>
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
   import DetailNavBar from 'views/detail/childComps/DetailNavBar'
   import DetailSwiper from 'views/detail/childComps/DetailSwiper'
+  import DetailBaseInfo from 'views/detail/childComps/DetailBaseInfo'
 
-  import {getDetail} from "network/detail"
+  import {getDetail,Goods} from "network/detail"
 
   export default {
     name: "detail",
     components: {
       DetailNavBar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo
     },
     data() {
       return {
         iid: null,
-        topImages: []
+        topImages: [],
+        goods:{}
       }
     },
     created() {
+      // 1.保存传入的iid
       this.iid = this.$route.params.iid
-
+      // 2.根据iid请求详情数据
       getDetail(this.iid).then(res => {
         // console.log(res);
         const data = res.result;
         console.log(data.itemInfo.topImages)
         this.topImages = data.itemInfo.topImages
+
+        // 3.获取商品信息
+        this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
       })
+
     }
   }
 
